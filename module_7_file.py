37caea7316e55d59eb2eda697340ce3711be8b9d
AlexZ-WP <alexz10@mail.ru>
2024-12-12 21:37:19 +0300

    Add files via upload

--- module_7_file.py
+++ module_7_file.py
@@ -1,26 +1,24 @@
 import os
 from datetime import datetime
-"""
-Освоить работу с файловой системой в Python, используя модуль os.
-Научиться применять методы os.walk, os.path.join, os.path.getmtime, os.path.dirname, os.path.getsize и использование 
-модуля time для корректного отображения времени.
-"""
+
 directory = '/Users/Alex/Documents/PycharmProjects/pythonProject/module_7'
-os.chdir('/Users/Alex/Documents/PycharmProjects/pythonProject/module_7')
+
 
 file = [f for f in os.listdir(directory) if os.path.isfile(f)]
 print(file)
-
 dir_ = [d for d in os.listdir() if os.path.isdir(d)]
 print(dir_)
 
 for root, dirs, files in os.walk(directory):
     for file in files:
-        filepath = os.path.join(directory, file)
-        # filetime = os.path.getmtime(file)
-        # formatted_ftime = datetime.fromtimestamp(filetime)
-        # filesize = os.stat(file).st_size
-        # parent_dir = directory
+        filepath = os.path.join(root, file)
+        filetime = os.path.getmtime(filepath) # указывается полный путь filepath
+        formatted_ftime = datetime.fromtimestamp(filetime)
+        filesize = os.stat(filepath).st_size
+        parent_dir = root
 
-        print(file)
-        print(filepath)
+    print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_ftime},'
+            f' Родительская директория: {parent_dir}')
+
+print(root, dirs, files)
+print(filepath)
